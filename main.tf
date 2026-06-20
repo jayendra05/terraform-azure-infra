830360ad4fa0d24383da528b78b042bb7c1e1323
Jayendra <jayendra06mishra@gmail.com>
2026-06-21 02:21:35 +0530

    adding one test rg

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 module "rg_dev" {
-  source = "./modules/rg"
+  source              = "./modules/rg"
   resource_group_name = var.rg_dev_name
-  location = var.rg_dev_location
+  location            = var.rg_dev_location
 }
 
 module "rg_prod" {
-  source = "./modules/rg"
+  source              = "./modules/rg"
   resource_group_name = var.rg_prod_name
-  location = var.rg_prod_location
+  location            = var.rg_prod_location
 }
 
 module "rg_staging" {
-  source = "./modules/rg"
+  source              = "./modules/rg"
   resource_group_name = var.rg_staging_name
-  location = var.rg_staging_location
+  location            = var.rg_staging_location
 }
 
 module "rg_test" {
-  source = "./modules/rg"
+  source              = "./modules/rg"
   resource_group_name = var.rg_test_name
-  location = var.rg_test_location
+  location            = var.rg_test_location
 }
