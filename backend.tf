2a2aa180b1b0393993ff6f36654b449370855b25
Jayendra <jayendra06mishra@gmail.com>
2026-06-21 00:35:00 +0530

    adding git workflow and updaing backend file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "azurerm" {
 
-    resource_group_name  = "rg-terraform-state"
+    resource_group_name  = "rg-jay-01"
     storage_account_name = "storageaccountjay01"
     container_name       = "terraform-state-file"
     key                  = "dev.terraform.tfstate"
--- provider.tf
+++ provider.tf
@@ -2,11 +2,9 @@ terraform {
 
   required_version = ">= 1.6"
   required_providers {
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
-
     }
   }
 }
