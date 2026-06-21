d5f0ee61cb05ec95097d643cba9f8f27814428bb
Jayendra <jayendra06mishra@gmail.com>
2026-06-22 01:29:02 +0530

    Adding nsg and nsg_association

---
+++ modules/nsg/variables.tf
@@ -0,0 +1,14 @@
+variable "nsg_name" {
+
+  type = string
+}
+
+variable "location" {
+
+  type = string
+}
+
+variable "resource_group_name" {
+
+  type = string
+}
---
+++ modules/nsg_association/outputs.tf
@@ -0,0 +1,4 @@
+output "association_id" {
+
+  value = azurerm_subnet_network_security_group_association.this.id
+}
