880d43f1850fb6a33c2955d9df6c62de379f4e03
Jayendra <jayendra06mishra@gmail.com>
2026-06-21 14:24:35 +0530

    fix error and change few code

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "this" {
 
   name                = var.vnet_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_namegit 
   address_space       = var.address_space
 
 }
