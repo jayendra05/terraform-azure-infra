f9e669e265b363f25187273b3c1b02f352ce3552
Jayendra <jayendra06mishra@gmail.com>
2026-06-21 17:29:23 +0530

    vnet-peering dev-test

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,29 +1,45 @@
+# Resource Groups
+
 rg_dev_name     = "rg-dev-001"
 rg_dev_location = "Central India"
-
 
 rg_prod_name     = "rg-prod-001"
 rg_prod_location = "West Europe"
 
-
 rg_staging_name     = "rg-stg-001"
 rg_staging_location = "West Europe"
 
-
-rg_test_us_01_name     = "rg-test_us_01"
+rg_test_us_01_name     = "rg-test-us-01"
 rg_test_us_01_location = "East US"
 
 
-vnet_dev_001_name     = "vnet-dev"
-address_dev_001_space = ["10.0.0.0/16"]
+# VNets
 
-vnet_test_us_01_name     = "vnet-test_us_01"
-address_test_us_01_space = ["192.0.0.0/16"]
+vnet_dev_001_name = "vnet-dev-001"
 
-
-subnet_dev_name   = "subnet-dev"
-subnet_dev_prefix = ["10.0.1.0/24"]
+address_dev_001_space = [
+  "10.0.0.0/16"
+]
 
 
-subnet_test_name   = "subnet-test"
-subnet_test_prefix = ["192.0.1.0/24"]
+vnet_test_us_01_name = "vnet-test-us-01"
+
+address_test_us_01_space = [
+  "192.168.0.0/16"
+]
+
+
+# Subnets
+
+subnet_dev_name = "subnet-dev"
+
+subnet_dev_prefix = [
+  "10.0.1.0/24"
+]
+
+
+subnet_test_name = "subnet-test"
+
+subnet_test_prefix = [
+  "192.168.1.0/24"
+]
--- variables.tf
+++ variables.tf
@@ -1,24 +1,71 @@
-variable "rg_dev_name" {}
-variable "rg_dev_location" {}
+# Resource Groups
 
-variable "rg_prod_name" {}
-variable "rg_prod_location" {}
+variable "rg_dev_name" {
+  type = string
+}
 
-variable "rg_staging_name" {}
-variable "rg_staging_location" {}
+variable "rg_dev_location" {
+  type = string
+}
 
-variable "vnet_dev_001_name" {}
-variable "address_dev_001_space" {}
+variable "rg_prod_name" {
+  type = string
+}
+
+variable "rg_prod_location" {
+  type = string
+}
+
+variable "rg_staging_name" {
+  type = string
+}
+
+variable "rg_staging_location" {
+  type = string
+}
+
+variable "rg_test_us_01_name" {
+  type = string
+}
+
+variable "rg_test_us_01_location" {
+  type = string
+}
 
 
-variable "rg_test_us_01_name" {}
-variable "rg_test_us_01_location" {}
+# VNets
 
-variable "vnet_test_us_01_name" {}
-variable "address_test_us_01_space" {}
+variable "vnet_dev_001_name" {
+  type = string
+}
 
-variable "subnet_dev_name" {}
-variable "subnet_dev_prefix" {}
+variable "address_dev_001_space" {
+  type = list(string)
+}
 
-variable "subnet_test_name" {}
-variable "subnet_test_prefix" {}
+variable "vnet_test_us_01_name" {
+  type = string
+}
+
+variable "address_test_us_01_space" {
+  type = list(string)
+}
+
+
+# Subnets
+
+variable "subnet_dev_name" {
+  type = string
+}
+
+variable "subnet_dev_prefix" {
+  type = list(string)
+}
+
+variable "subnet_test_name" {
+  type = string
+}
+
+variable "subnet_test_prefix" {
+  type = list(string)
+}
