ac7ba18b79d9366909ade0f1ea055ae2754f8b02
Jayendra <jayendra06mishra@gmail.com>
2026-06-23 00:27:16 +0530

    added disk_attachment and managed disk module

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,8 +1,17 @@
+#############################################
+# VM ID
+#############################################
+
 output "vm_id" {
 
   value = azurerm_linux_virtual_machine.this.id
 
 }
+
+
+#############################################
+# VM Name
+#############################################
 
 output "vm_name" {
 
@@ -10,20 +19,68 @@ output "vm_name" {
 
 }
 
-output "vm_private_ip" {
 
-  value = azurerm_linux_virtual_machine.this.private_ip_address
+#############################################
+# VM Size
+#############################################
+
+output "vm_size" {
+
+  value = azurerm_linux_virtual_machine.this.size
 
 }
 
-output "vm_public_ip" {
 
-  value = azurerm_linux_virtual_machine.this.public_ip_address
+#############################################
+# VM Location
+#############################################
+
+output "vm_location" {
+
+  value = azurerm_linux_virtual_machine.this.location
 
 }
 
-output "vm_id" {
 
-  value = azurerm_linux_virtual_machine.this.id
+#############################################
+# Network Interface ID
+#############################################
+
+output "network_interface_ids" {
+
+  value = azurerm_linux_virtual_machine.this.network_interface_ids
+
+}
+
+
+#############################################
+# Computer Name
+#############################################
+
+output "computer_name" {
+
+  value = azurerm_linux_virtual_machine.this.computer_name
+
+}
+
+
+#############################################
+# Admin Username
+#############################################
+
+output "admin_username" {
+
+  value = azurerm_linux_virtual_machine.this.admin_username
+
+}
+
+
+#############################################
+# OS Disk ID
+#############################################
+
+output "os_disk_id" {
+
+  value = azurerm_linux_virtual_machine.this.os_disk_id
 
 }
