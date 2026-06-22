4230cdbf3b80986e9568d41b163b4a692463141f
Jayendra <jayendra06mishra@gmail.com>
2026-06-22 21:39:33 +0530

    adding-port-8080-dev-ci-001

--- main.tf
+++ main.tf
@@ -131,6 +131,35 @@ module "nsg_dev_ci_001" {
 
 
 #############################################
+# EXTRA NSG RULE ONLY FOR DEV
+#############################################
+
+resource "azurerm_network_security_rule" "allow_8080_dev_ci_001" {
+
+  name                        = "Allow-8080"
+
+  priority                    = 130
+
+  direction                   = "Inbound"
+
+  access                      = "Allow"
+
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+
+  destination_port_range      = "8080"
+
+  source_address_prefix       = "*"
+
+  destination_address_prefix  = "*"
+
+  resource_group_name         = module.rg_dev_ci_001.resource_group_name
+
+  network_security_group_name = module.nsg_dev_ci_001.nsg_name
+}
+
+#############################################
 # NSG ASSOCIATION
 #############################################
 
