bf696889db258eaead5a1644fcf06170c21ae55f
Jayendra <jayendra06mishra@gmail.com>
2026-06-23 03:35:03 +0530

    Allow-port-8000

--- main.tf
+++ main.tf
@@ -187,6 +187,31 @@ resource "azurerm_network_security_rule" "allow_8080_dev_ci_001" {
   network_security_group_name = module.nsg_dev_ci_001.nsg_name
 }
 
+resource "azurerm_network_security_rule" "allow_8000_dev_ci_001" {
+
+  name = "Allow-8000"
+
+  priority = 140
+
+  direction = "Inbound"
+
+  access = "Allow"
+
+  protocol = "Tcp"
+
+  source_port_range = "*"
+
+  destination_port_range = "8000"
+
+  source_address_prefix = "*"
+
+  destination_address_prefix = "*"
+
+  resource_group_name = module.rg_dev_ci_001.resource_group_name
+
+  network_security_group_name = module.nsg_dev_ci_001.nsg_name
+}
+
 #############################################
 # NSG ASSOCIATION
 #############################################
